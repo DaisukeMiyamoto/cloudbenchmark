1caaaa5369405ca0c98a652c5cd69e89183d1f66
Daisuke Miyamoto <midaisuk@gmail.com>
2018-11-28 04:43:23 +0000

    update test for keymanager

--- tests/test_deploy_manager.py
+++ tests/test_deploy_manager.py
@@ -2,18 +2,11 @@
 import os
 import time
 import cloudbenchmark
-from cloudbenchmark.deploy_manager import deploy_core
+from cloudbenchmark.deploy_manager import deploy_multi
 
 
 def test_deploy(instance_type='t2.micro'):
+    deploy_multi('test-cpu', 'midaisuk-benchmarks', 1, 'ap-northeast-1')
 
-    deploy = {
-        'instance_type': 't2.micro',
-        'job_type': 'ec2-sysbench-cpu',
-        'job_size': 'small',
-        'key_name': 'aws-daisuke-tokyo',
-        'local_key_path': '~/.ssh/aws-daisuke-tokyo.pem',
-        'output_bucket_name': 'midaisuk-benchmarks',
-        'region': 'ap-northeast-1'
-    }
-    deploy_core(deploy)
+def test_deploy_multi(instance_type='t2.micro'):
+    deploy_multi('ec2-sysbench-cpu-small', 'midaisuk-benchmarks', 2, 'ap-northeast-1')
